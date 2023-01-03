e2fb067f4d717df66839eae6f080d4d961d9abfd
Vasiliev781410 <37668858+Vasiliev781410@users.noreply.github.com>
2023-01-03 21:46:36 +0200

    95%

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,6 @@
 const URL = "https://restcountries.com/v3.1/name/";
 
-function fetchCountries(name){     
+function fetchCountries(name){        
     return fetch(URL+name);
 };
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './css/styles.css';
 import {fetchCountries} from "./fetchCountries.js";
 import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 
 const inp = document.querySelector("#search-box");
-const btnStart = document.querySelector("#start");
 const countryList = document.querySelector(".country-list");
 
 function inputResult(data){
@@ -14,7 +14,7 @@ function inputResult(data){
   if (data.length < 2) {  
     
   };
-  const markup = data.map((country) => `<li class="country__item"><img class="country__image" src=${country.flags.png} alt="flag of "+${country.altSpellings[1]} width="30" height="30"> <span> ${country.name.common}</span></li>`).join("");
+  const markup = data.map((country) => `<li class="country__item"><img class="country__image" src=${country.flags.svg} alt="flag of "+${country.altSpellings[1]} width="30" height="30"> <span> ${country.name.common}</span></li>`).join("");
   countryList.insertAdjacentHTML("beforeend", markup);
 
   countryItems = countryList.children;
@@ -31,27 +31,35 @@ function inputResult(data){
     countryItem.style.marginBottom = "10px";
   };     
 };
-         
 
-function search(){     
-   
-    fetchCountries(inp.value)
+let name = "";
+
+function search(){ 
+  name = inp.value.trim();
+  console.log(name);    
+  if (name === ""){
+    countryList.innerHTML = "";
+    return;
+  }     
+    fetchCountries(name)
       .then(response => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
+        if (!response.ok) {      
+          throw new Error(response.status);                    
+        }        
         return response.json();
       })
       .then(data => {              
         if (data.length > 10) {
           Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+          countryList.innerHTML = "";
           return;
         };
         inputResult(data);     
       })
       .catch(error => {
-        // Error handling
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+        countryList.innerHTML = "";
       });
 };
 
-btnStart.addEventListener("click",search);
+inp.addEventListener("input",debounce(search,DEBOUNCE_DELAY));
